f6c07452d33a262c3f4a76a57e5e3775cbc57d47
dzodmitry <hwapedro@list.ru>
2020-12-23 20:49:01 +0700

    lol

--- src/global/constant.ts
+++ src/global/constant.ts
@@ -3,7 +3,7 @@ import avatar1 from '../assets/img/profile/01.png'
 import avatar2 from '../assets/img/profile/02.png'
 import avatar3 from '../assets/img/profile/03.png'
 
-export const API_URL = 'https://server.learnik.fun/api/v1'
+export const API_URL = 'https://server.learnik.fun/'
 export const AVATARS = [
     { path: avatar0 },
     { path: avatar1 },
